7a97de68600210ad4cfba13ecb35a351e25077e3
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-25 22:37:28 +0545

    server

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
-const z = 10;
+// const z = 10;
 
-const read = (text: string) => {
-  return text;
-};
+// const read = (text: string) => {
+//   return text;
+// };
 
-const msg = read("Hello");
-console.log(msg);
+// const msg = read("Hello");
+// console.log(msg);
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    return new Response("hellooo<<");
+  },
+});
+
+console.log(`${server.port}`);
