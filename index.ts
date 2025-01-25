dd869fd5cfc7eef3f8416c40f6de0b61844ed311
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-25 22:50:15 +0545

    simple routes

--- index.ts
+++ index.ts
@@ -12,8 +12,30 @@ import figlet from "figlet";
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    const body = figlet.textSync("Hey");
-    return new Response(body);
+    const url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      const body = figlet.textSync("Hey");
+      return new Response(body);
+    }
+
+    if (url.pathname === "/about") {
+      return new Response("About me");
+    }
+
+    if (url.pathname === "/feed") {
+      throw new Error("Could not fetch feed");
+    }
+
+    return new Response("404");
+  },
+
+  error(error) {
+    return new Response(`<pre> ${error} \n ${error.stack} </pre>`, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   },
 });
 
