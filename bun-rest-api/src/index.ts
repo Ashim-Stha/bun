ad7a853f5e5c747c97ef43f0929f7ba4589955cf
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-26 11:39:53 +0545

    routes

--- bun-rest-api/src/index.ts
+++ bun-rest-api/src/index.ts
@@ -4,7 +4,35 @@ import { plugin } from "./plugin";
 //APPLICATION
 const app = new Elysia()
   .get("/", () => "Hello Luffy")
+  .get("/post/:id", ({ params: { id } }) => {
+    return { id: id, title: "learn bun" };
+  })
+  .post("/post", ({ body, set }) => {
+    set.status = 201;
+    return body;
+  })
+  .post("/context", ({ body }) => {
+    return body;
+  })
+  .get("/track/*", () => {
+    return "Track route";
+  })
+  .get("/tracks", () => {
+    // return new Response(
+    //   JSON.stringify({
+    //     tracks: ["Dancing Feat", "Sam I"],
+    //   }),
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
 
+    return {
+      tracks: ["Dancing Feat", "Sam I"],
+    };
+  })
   .listen(3000);
 
 console.log(
